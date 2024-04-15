39137eac3597b83c9b4625138a4b9a88ee6298ba
PalomaValiente <thf11032@educastur.es>
2024-04-15 16:05:59 +0200

    Añadido esqueleto Spring Boot

---
+++ src/test/java/com/ciber04/calculator04/Calculator04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber04.calculator04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
