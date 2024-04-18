a97e7ceca2c7c6a941a22e440aac6e80ea0fa1ce
PalomaValiente <thf11032@educastur.es>
2024-04-18 19:33:16 +0200

    Se añaden suma, controlador y test unitarios

---
+++ src/main/java/com/ciber04/calculator04/Calculator04Application.java
@@ -0,0 +1,13 @@
+package com.ciber04.calculator04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator04Application.class, args);
+	}
+
+}
